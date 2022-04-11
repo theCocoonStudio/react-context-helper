f565b9f8c3992ae08fbe27dbfcce5ea4a16f03ad
Izzy Erlich <izzy@thecocoon.studio>
2022-04-10 21:21:36 -0400

    add reducer option

--- src/components/ContextHelper.js
+++ src/components/ContextHelper.js
@@ -1,6 +1,6 @@
 import { useCallback, useContext, memo, useState, useMemo } from "react";
 import { enableES5 } from "immer";
-import { useImmer } from "use-immer";
+import { useImmer, useImmerReducer } from "use-immer";
 import React from "react";
 enableES5();
 
@@ -40,6 +40,23 @@ export const ContextProvider = ({ value, contextObj, children }) => {
   );
 };
 
+export const ContextReducerProvider = ({
+  value,
+  contextObj,
+  children,
+  reducer,
+}) => {
+  const [context, dispatch] = useImmerReducer(reducer, value);
+
+  const contextValue = useMemo(
+    () => ({ ...context, dispatch }),
+    [context, dispatch],
+  );
+  return (
+    <contextObj.Provider value={contextValue}>{children}</contextObj.Provider>
+  );
+};
+
 export const useMemoConsumer = (
   ChildComponent,
   ContextObj,
@@ -56,50 +73,3 @@ export const useMemoConsumer = (
 
   return [Memo, contextProps];
 };
-
-//usage:
-// const [MemoizedChildComponent, contextProps] = useContext(...)
-
-//return <MemoizedChildComponent {...contextProps} {..any other props}><ChildComponent/></MemoizedChildComponent>
-
-//write cli that takes a component and determines if it's better to use React.memo or not
-
-//context objects will be exported using a named export in the same file the ContextProvider is used:
-
-//App.js
-/*
-import ContextProvider from 'react-context-helper';
-
-export const Context = React.createContext();
-
-export default function App() {
-  const initialValue = {
-    foo: bar,
-    fizz: buzz,
-  };
-  return (
-    <ContextProvider value={initialValue} contextObj={Context}>
-      <User />
-    </ContextProvider>
-  )
-}
-*/
-
-//Consumer.js
-/*
-import { useMemoizedChild } from "react-context-helper";
-import Context from "/path/to/App.js";
-import ChildComponent from "/path/to/ChildComponent.js";
-
-export default function Consumer = () => {
-const [MemoizedChildComponent, contextProps] = 
-  useMemoConsumer(ChildComponent, Context, ["foo"]);
-
-return 
-  <MemoizedChildComponent {...contextProps} {..any other props}/> 
-}
-*/
-
-//TO DO :
-
-//test context imports
