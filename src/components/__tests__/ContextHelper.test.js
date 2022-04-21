99f85f45320e8a25de003f6cafea2d568e968d38
Izzy Erlich <izzy@thecocoon.studio>
2022-04-20 20:28:17 -0400

    complete initial test suite

--- src/components/__tests__/ContextHelper.test.js
+++ src/components/__tests__/ContextHelper.test.js
@@ -6,6 +6,7 @@ import {
 import { createContext, useContext, useState, useCallback } from "react";
 import { mount } from "enzyme";
 import React from "react";
+import { Profiler } from "react/cjs/react.production.min";
 
 const LightComponentPure = ({ propTwo, fibNum, nonContextProp }) => {
   return (
@@ -15,6 +16,12 @@ const LightComponentPure = ({ propTwo, fibNum, nonContextProp }) => {
     </>
   );
 };
+//slow fibonacci implementation
+function fib(x) {
+  if (x <= 0) return 0;
+  if (x == 1) return 1;
+  return fib(x - 1) + fib(x - 2);
+}
 
 test("ContextProvider: context is accessible with class components", () => {
   const contextObj = createContext({});
@@ -237,12 +244,7 @@ describe("testing useMemoConsumer hook", () => {
   test("MemoizedConsumer doesn't update when unconnected context is changed, but regular consumer does render", () => {
     let timesRendered = 0;
     let timesRendered2 = 0;
-    //slow fibonacci implementation
-    function fib(x) {
-      if (x <= 0) return 0;
-      if (x == 1) return 1;
-      return fib(x - 1) + fib(x - 2);
-    }
+
     const HeavyComponent = ({ contextObj }) => {
       timesRendered2 += 1;
       const { fibNum } = useContext(contextObj);
@@ -326,4 +328,160 @@ describe("testing useMemoConsumer hook", () => {
     expect(wrapper2.find("#test").text()).toBe("20");
     expect(timesRendered2).toBe(2);
   });
+});
+
+test("ensure using useMemoConsumer hook saves computation time", () => {
+  const ContextObj = createContext({});
+  const times = [0, 0, 0, 0];
+  const HeavyComponent = () => {
+    const { fibNum } = useContext(ContextObj);
+    const answer = fib(fibNum);
+    return <div>{answer}</div>;
+  };
+
+  const HeavyComponentPure = ({ fibNum }) => {
+    const answer = fib(fibNum);
+    return <div>{answer}</div>;
+  };
+
+  const LightComponent = () => {
+    const { fibNum } = useContext(ContextObj);
+    return <div>{fibNum}</div>;
+  };
+
+  const LightComponentPure = ({ fibNum }) => {
+    return <div>{fibNum}</div>;
+  };
+
+  const Child = ({ Component }) => {
+    const context = useContext(ContextObj);
+    const update = () => {
+      context.updateContext({
+        propOne: context.propOne + 1,
+      });
+    };
+    return (
+      <div id="click" onClick={() => update()}>
+        <Component />
+      </div>
+    );
+  };
+  const PureChild = ({ Component }) => {
+    const [MemoizedConsumer, contextProps] = useMemoConsumer(
+      Component,
+      ContextObj,
+      ["fibNum"],
+    );
+    const context = useContext(ContextObj);
+    const update = () => {
+      context.updateContext({
+        propOne: context.propOne + 1,
+      });
+    };
+    return (
+      <div id="click" onClick={() => update()}>
+        <MemoizedConsumer {...contextProps} />
+      </div>
+    );
+  };
+
+  const ProfilerWrapper = ({ Child, Component, componentIndex, id }) => {
+    const callback = (
+      id, // the "id" prop of the Profiler tree that has just committed
+      phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+      actualDuration, // time spent rendering the committed update
+      baseDuration, // estimated time to render the entire subtree without memoization
+      startTime, // when React began rendering this update
+      commitTime, // when React committed this update
+      interactions, // the Set of interactions belonging to this update
+    ) => {
+      times[componentIndex] = times[componentIndex] + actualDuration;
+    };
+    return (
+      <Profiler id={id} onRender={callback}>
+        <Child Component={Component} />
+      </Profiler>
+    );
+  };
+
+  const wrapper = mount(
+    <ProfilerWrapper
+      Child={Child}
+      Component={LightComponent}
+      componentIndex={0}
+      id="LightComponent"
+    />,
+    {
+      wrappingComponent: ContextProvider,
+      wrappingComponentProps: {
+        contextObj: ContextObj,
+        value: { propOne: 0, fibNum: "20" },
+      },
+    },
+  );
+  const wrapper2 = mount(
+    <ProfilerWrapper
+      Child={PureChild}
+      Component={LightComponentPure}
+      componentIndex={1}
+      id="LightComponentPure"
+    />,
+    {
+      wrappingComponent: ContextProvider,
+      wrappingComponentProps: {
+        contextObj: ContextObj,
+        value: { propOne: 0, fibNum: "20" },
+      },
+    },
+  );
+  const wrapper3 = mount(
+    <ProfilerWrapper
+      Child={Child}
+      Component={HeavyComponent}
+      componentIndex={2}
+      id="HeavyComponent"
+    />,
+    {
+      wrappingComponent: ContextProvider,
+      wrappingComponentProps: {
+        contextObj: ContextObj,
+        value: { propOne: 0, fibNum: "20" },
+      },
+    },
+  );
+  const wrapper4 = mount(
+    <ProfilerWrapper
+      Child={PureChild}
+      Component={HeavyComponentPure}
+      componentIndex={3}
+      id="HeavyComponentPure"
+    />,
+    {
+      wrappingComponent: ContextProvider,
+      wrappingComponentProps: {
+        contextObj: ContextObj,
+        value: { propOne: 0, fibNum: "20" },
+      },
+    },
+  );
+  wrapper.update();
+  wrapper2.update();
+  wrapper3.update();
+  wrapper4.update();
+
+  for (let i = 0; i < 10; i++) {
+    wrapper.find("#click").simulate("click");
+    wrapper2.find("#click").simulate("click");
+    wrapper3.find("#click").simulate("click");
+    wrapper4.find("#click").simulate("click");
+  }
+  console.log(
+    "Each component undergoes initial render and then a context property that it is not using is updated 1000 times:",
+  );
+  console.log("LightComponent total rendering time: " + times[0] + "s");
+  console.log("LightComponentPure total rendering time: " + times[1] + "s");
+  console.log("HeavyComponent total rendering time: " + times[2] + "s");
+  console.log("HeavyComponentPure total rendering time: " + times[3] + "s");
+
+  expect(times[3]).toBeLessThan(times[2]);
 });
